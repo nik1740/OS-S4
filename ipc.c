fa54dd222b541cc4bab09ee36e2865abca437b4b
Nikhil S <129724432+nik1740@users.noreply.github.com>
2025-04-06 22:17:14 +0530

    Add files via upload

---
+++ ipc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#define SHM_SIZE 1024
+void main(){
+char *shm, *ptr;
+key_t key = ftok("shmfile.txt",'R');
+int shmid = shmget(key, SHM_SIZE, IPC_CREAT |0666); //creating shared memory segment
+shm = shmat(shmid, NULL, 0); //attach shmptr = shm;
+sprintf(ptr, "Shared Memory Content");
+printf("Data Written To Shared Memory\n");ptr = shm;
+printf("Data In The SHM: ");while(*ptr != '\0'){
+printf("%c", *ptr);ptr++;
+}
+shmdt(shm); //detach shm
+shmctl(shmid, IPC_RMID, NULL);
+}
